0bea3be93bcef7ad1abb72ef1303f056f18aac6f
Rebeca Anahí Machado <anahimachado90@gmail.com>
2026-09-19 12:56:33 -0300

    Se agrega #backend de la pagina de la panaderia

--- acceso_a_datos.py
+++ acceso_a_datos.py
@@ -1,1 +1,18 @@
 #backend de la pagina de la panaderia
+from conexion import conectar
+
+def obtener_productos():
+    """Consulta y devuelve todos los productos de nuestra base de datos."""
+    conexion = conectar()
+    productos = []
+    if conexion:
+        try:
+            cursor = conexion.cursor()
+            # Seleccionamos Código, Nombre, Categoría, Stock y Precio
+            cursor.execute("SELECT id_producto, nombre, categoria, stock, precio_actual FROM producto")
+            productos = cursor.fetchall()
+        except Exception as e:
+            print(f"Error al obtener productos: {e}")
+        finally:
+            conexion.close()
+    return productos
