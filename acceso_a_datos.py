585b76fd58006f065de8932e83cfee6ac42ee3e1
Rebeca Anahí Machado <anahimachado90@gmail.com>
2026-09-20 13:01:33 -0300

    Se modifica Front

--- acceso_a_datos.py
+++ acceso_a_datos.py
@@ -1,14 +1,12 @@
-#backend de la pagina de la panaderia
 from conexion import conectar
 
 def obtener_productos():
-    """Consulta y devuelve todos los productos de nuestra base de datos."""
+    """Consulta y devuelve todos los productos de la base de datos."""
     conexion = conectar()
     productos = []
     if conexion:
         try:
             cursor = conexion.cursor()
-            # Seleccionamos Código, Nombre, Categoría, Stock y Precio
             cursor.execute("SELECT id_producto, nombre, categoria, stock, precio_actual FROM producto")
             productos = cursor.fetchall()
         except Exception as e:
@@ -16,3 +14,19 @@ def obtener_productos():
         finally:
             conexion.close()
     return productos
+
+def obtener_clientes():
+    """Consulta y devuelve todos los clientes de la base de datos."""
+    conexion = conectar()
+    clientes = []
+    if conexion:
+        try:
+            cursor = conexion.cursor()
+            # Seleccionamos Nombre, Apellido, Email, Teléfono y Dirección
+            cursor.execute("SELECT nombre, apellido, email, telefono, direccion FROM cliente")
+            clientes = cursor.fetchall()
+        except Exception as e:
+            print(f"Error al obtener clientes: {e}")
+        finally:
+            conexion.close()
+    return clientes
